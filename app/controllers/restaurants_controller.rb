ea1eac82d82a02668f049ac3e047833f34a4ae35
T-erry <terrywachuka10@gmail.com>
2023-03-20 12:46:14 +0300

    refactored the index action

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,11 +2,11 @@ class RestaurantsController < ApplicationController
 rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
     def index 
         restaurants = Restaurant.all
-        render json: restaurants, status: :accepted
+        render json: restaurants, except: [:created_at, :updated_at], status: :accepted
     end
     def show 
         restaurant = Restaurant.find(params[:id])
-        render json: restaurant, status: :created
+        render json: restaurant, except: [:created_at, :updated_at], include: { pizzas: { except: [:created_at, :updated_at] } }, status: :created
     end
     def destroy 
         restaurant = Restaurant.find(params[:id])

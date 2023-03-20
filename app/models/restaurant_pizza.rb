517aba97cc34eb08896de1eee1a5c5f29716fafa
T-erry <terrywachuka10@gmail.com>
2023-03-20 12:47:39 +0300

    created a custom validation

--- app/models/restaurant_pizza.rb
+++ app/models/restaurant_pizza.rb
@@ -5,5 +5,12 @@ class RestaurantPizza < ApplicationRecord
   validates :pizza_id, presence: true
   validates :restaurant_id, presence: true
   validates :price, numericality: { greater_than: 0, less_than_or_equal_to: 30 }
+  validate  :price_should_be_unique
 
+  private 
+  def price_should_be_unique
+    if RestaurantPizza.where("price = ?" )
+      errors.add(:price, "has already been taken")
+    end
+end
 end
